9117eef68062087a7ab9233344d9570222817c2b
Daniel Christiansen <dannyjameschrist@gmail.com>
2022-02-01 10:55:55 -0500

    new styles

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -9,7 +9,11 @@ import {
 } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 
+import useStyles from './styles';
+
 const Product = ({ product }) => {
+  const classes = useStyles();
+  
   return (
     <Card className={classes.root}>
       <CardMedia className={classes.media} image='' title={product.name} />
