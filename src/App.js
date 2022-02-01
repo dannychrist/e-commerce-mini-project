02bfa29d0d58c86b90429cb8dbb1c5fc81285009
Daniel Christiansen <dannyjameschrist@gmail.com>
2022-02-01 13:21:12 -0500

    commerce.js added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
-
-// import Products from './components/Products/Products';
-// import NavBar from './components/NavBar/NavBar';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
 
 import { Products, NavBar } from './components';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
   return (
     <div>
       <NavBar />
