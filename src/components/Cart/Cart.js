f396d388154d9384875ed0cbb18dbb3ff08ddd95
Daniel Christiansen <dannyjameschrist@gmail.com>
2022-02-25 11:35:27 -0500

    new changes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,22 +6,22 @@ import useStyles from './cartStyles';
 
 const Cart = ({ cart }) => {
   const classes = useStyles();
-  const isEmpty = !cart.line_items.length;
 
-  const EmptyCart = () => {
+  const EmptyCart = () => (
     <Typography variant='subtitle1'>
       You have no items in your shopping cart, start adding some!
-    </Typography>;
-  };
+    </Typography>
+  );
 
-  const FilledCart = () => {
+  const FilledCart = () => (
     <>
-      <Grid container spacing={3}></Grid>
-      {cart.line_items.map((item) => (
-        <Grid item xs={12} sm={4} key={item.id}>
-          <div>{item.name}</div>
-        </Grid>
-      ))}
+      <Grid container spacing={3}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <div>{item.name}</div>
+          </Grid>
+        ))}
+      </Grid>
       <div className={classes.cardDetails}>
         <Typography variant='h4'>
           Subtotal: {cart.subtotal.formatted_with_symbol}
@@ -47,8 +47,10 @@ const Cart = ({ cart }) => {
           </Button>
         </div>
       </div>
-    </>;
-  };
+    </>
+  );
+
+  if (!cart.line_items) return 'Loading...';
 
   return (
     <Container>
@@ -56,7 +58,7 @@ const Cart = ({ cart }) => {
       <Typography className={classes.title} variant='h3'>
         Your Shopping Cart
       </Typography>
-      {isEmpty ? <EmptyCart /> : <FilledCart />}
+      {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
     </Container>
   );
 };
