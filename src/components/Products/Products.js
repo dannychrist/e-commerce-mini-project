f1531ecb0a2b9b124f2efb836d5c27aa69a715d5
Daniel Christiansen <dannyjameschrist@gmail.com>
2022-03-05 11:25:45 -0500

    new changes

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './productsStyles';
 
 const Products = ({ products, onAddToCart }) => {
+  const classes = useStyles();
+
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justifyContent='center' spacing={4}>
         {products.map((product) => {
           return (
